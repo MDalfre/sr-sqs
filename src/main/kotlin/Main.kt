346ed4ba8f20cc81cd9069ec66c3a1771c6d70f5
Marcio Dalfre <marciodalfre@gmail.com>
2021-08-11 12:38:55 -0300

    New features:
    - New Theme;
    - Create Queues;
    - Create init.sh for localstack;
    - Small corrections.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -28,7 +28,6 @@ fun main() = Window(
             Modifier.background(backgroundBlue)
         ) {
             mainView(logService)
-
         }
         Column {
             topBar(logService)
--- src/main/kotlin/components/TopBar.kt
+++ src/main/kotlin/components/TopBar.kt
@@ -2,11 +2,29 @@ package components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -18,6 +36,7 @@ import androidx.compose.ui.window.v1.DialogProperties
 import commons.backgroundBlue
 import commons.orange
 import connectionService
+import service.FileHandleService
 import service.GenericSqsService
 import service.LogService
 
@@ -49,59 +68,66 @@ fun topBar(logService: LogService) {
             ) {
                 DropdownMenuItem(
                     modifier = Modifier.padding(5.dp).height(15.dp),
+                    enabled = (connectionService != null),
                     onClick = {
                         expanded = !expanded
                         createQueue = !createQueue
                     }
                 ) {
-                    Text("Menu 1", style = TextStyle(fontSize = 10.sp))
+                    Text("Create Queue", style = TextStyle(fontSize = 10.sp))
                 }
+                Divider()
                 DropdownMenuItem(
                     modifier = Modifier.padding(5.dp).height(15.dp),
+                    enabled = (connectionService != null),
                     onClick = {
-
+                        expanded = !expanded
+                        if (connectionService != null) {
+                            val queues = GenericSqsService(connectionService!!, logService).getQueues()
+                            FileHandleService().createFile(queues)
+                        }
                     }
                 ) {
-                    Text("Menu 2", style = TextStyle(fontSize = 10.sp))
+                    Text("Create localstack init.sh", style = TextStyle(fontSize = 10.sp))
                 }
-                DropdownMenuItem(
-                    modifier = Modifier.padding(5.dp).height(15.dp),
-                    onClick = {
-
-                    }
-                ) {
-                    Text("Menu 3", style = TextStyle(fontSize = 10.sp))
-                }
-
             }
 
             if (createQueue) {
                 Dialog(
-                    onDismissRequest = {},
-                    properties = DialogProperties(title = "Create Queue", IntSize(ALERT_WIDTH, ALERT_HEIGHT)),
+                    onDismissRequest = { createQueue = !createQueue },
+                    properties = DialogProperties(title = "Create Queue", IntSize(ALERT_WIDTH, ALERT_SMALL_HEIGHT)),
                     content = {
                         Column(
                             modifier = Modifier.background(backgroundBlue)
                                 .fillMaxSize(),
                         ) {
-                            Column {
+                            Column(
+                                modifier = Modifier.padding(50.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.SpaceEvenly
+                            ) {
                                 defaultTextEditor(
                                     text = "Queue name",
                                     value = createQueueName,
                                     onValueChange = { createQueueName = it }
                                 )
-                                Button(
-                                    modifier = buttonModifier,
-                                    enabled = (createQueueName.isNotEmpty() && connectionService != null),
-                                    colors = defaultButtonColor,
-                                    onClick = {
-                                        Thread {
-                                            GenericSqsService(connectionService!!, logService)
-                                                .createQueue(createQueueName)
-                                        }.start()
-                                    }
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceEvenly
                                 ) {
-                                    Text("Create Queue")
+                                    Button(
+                                        modifier = buttonModifier,
+                                        enabled = (createQueueName.isNotEmpty() && connectionService != null),
+                                        colors = defaultButtonColor,
+                                        onClick = {
+                                            Thread {
+                                                GenericSqsService(connectionService!!, logService)
+                                                    .createQueue(createQueueName)
+                                            }.start()
+                                        }
+                                    ) {
+                                        Text("Create Queue")
+                                    }
                                 }
                             }
                         }
